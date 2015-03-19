7a40874ba000b7875241f90f6211ad1929994eda
Jonathan Brachthaeuser <jonathan@b-studios.de>
2015-03-19 18:43:48 +0100

    Start from scratch

--- test/If.scala
+++
@@ -1,3 +0,0 @@
-class If {
-	def x(flag: Boolean) = if(flag) 1823 else false
-}
--- test/Q.scala
+++
@@ -1,7 +0,0 @@
-case class B(xyz: Int)
-trait Q {
-	def m(i: Int) = i
-	def m2(a: Int, b: Int) = a + b
-	def self = this
-}
-
